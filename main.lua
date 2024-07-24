9ea2b7e6b9a9b0e47ac719249925582d8b0f5ebb
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2024-07-24 13:46:14 +0800

    Initial commit

---
+++ main.lua
@@ -0,0 +1,112 @@
+-- fas-rs 使用它来提供指定版本的api
+-- fas-rs use this to provide version-specified api
+API_VERSION = 1
+--
+-- 中文:
+-- 该枚举对应的lua函数
+-- 在 fas-rs 中将被 fas-rs 回调
+-- pub enum CallBacks {
+--  LoadFas(pid_t, String), --------> function load_fas(pid, pkg)
+--  UnloadFas(pid_t, String), ------> function unload_fas(pid, pkg)
+--  StartFas, ----------------------> function start_fas()
+--  StopFas, -----------------------> function stop_fas()
+--  InitCpuFreq, -------------------> function init_cpu_freq()
+--  ResetCpuFreq, ------------------> function reset_cpu_freq()
+-- }
+--
+-- 可注册的函数说明:
+-- function load_fas(pid, pkg)
+-- 当 fas-rs 加载到目标游戏时调用，
+-- 参数为目标应用程序的pid和包名
+--
+-- function unload_fas(pid, pkg)
+-- 当 fas-rs 卸载到目标游戏时调用，
+-- 参数为目标应用程序的pid和包名
+--
+-- function start_fas()
+-- 切换到fas-rs工作状态时调用。
+--
+-- function stop_fas()
+-- 切换到 fas-rs 不工作状态时调用。
+--
+-- function init_cpu_freq()
+-- 当cpu控制器进入控制状态时调用。
+--
+-- function reset_cpu_freq()
+-- 当cpu控制器退出控制状态时调用。
+--
+-- 附加: 在函数外的lua代码会在插件加载时被执行，
+-- 如果你有执行初始化内容的需求，这样做很方便。
+--
+-- fas-rs提供的函数:
+-- log_info("message")
+-- 打印一个info等级日志到/sdcard/Android/fas-rs/fas_log.txt
+--
+-- log_error("message")
+-- 打印一个error等级日志到/sdcard/Android/fas-rs/fas_log.txt
+--
+-- log_debug("message")
+-- 打印一个debug等级日志到/sdcard/Android/fas-rs/fas_log.txt，
+-- 此等级在fas-rs的release build不开启
+--
+-- set_policy_freq_offset(policy, offset)
+-- 设置指定集群的fas频率偏移量，可为负数
+--
+-- ------------------------------------
+--
+-- EN:
+-- The lua function corresponding to this enumeration
+-- in fas-rs will be called back by fas-rs
+-- pub enum CallBacks {
+--  LoadFas(pid_t, String), --------> function load_fas(pid, pkg)
+--  UnloadFas(pid_t, String), ------> function unload_fas(pid, pkg)
+--  StartFas, ----------------------> function start_fas()
+--  StopFas, -----------------------> function stop_fas()
+--  InitCpuFreq, -------------------> function init_cpu_freq()
+--  ResetCpuFreq, ------------------> function reset_cpu_freq()
+-- }
+--
+-- Registerable function description:
+-- function load_fas(pid, pkg)
+-- Called when fas-rs is loaded into the target game,
+-- The parameters are the pid and package name of the target application
+--
+-- function unload_fas(pid, pkg)
+-- Called when fas-rs is unloaded to the target game,
+-- The parameters are the pid and package name of the target application
+--
+-- function start_fas()
+-- Called when switching to fas-rs working state.
+--
+-- function stop_fas()
+-- Called when switching to fas-rs not-working state.
+--
+-- function init_cpu_freq()
+-- Called when the cpu controller enters the control state.
+--
+-- function reset_cpu_freq()
+-- Called when the cpu controller exits the control state.
+--
+-- Extra: Lua code outside the function will be
+-- executed when the extension is loaded, if you need to
+-- execute initialization content, this is convenient.
+--
+-- Functions provided by fas-rs:
+-- log_info("message")
+-- Print an info level log to /sdcard/Android/fas-rs/fas_log.txt
+--
+-- log_error("message")
+-- Print an error level log to /sdcard/Android/fas-rs/fas_log.txt
+--
+-- log_debug("message")
+-- Print a debug level log to /sdcard/Android/fas-rs/fas_log.txt,
+-- This level is not enabled in the release build of fas-rs.
+--
+-- set_policy_freq_offset(policy, offset)
+-- Sets the FAS frequency offset for the specified cluster, which can be a negative number.
+--
+log_info("hello world") -- Initial
+
+function start_fas() -- A example for callback functions
+    log_info("fas started")
+end
---
+++ module/customize.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+#
+# Copyright 2023 shadow3aaa@gitbub.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+LOCALE=$(getprop persist.sys.locale)
+
+local_print() {
+	if [ $LOCALE = zh-CN ]; then
+		ui_print "$1"
+	else
+		ui_print "$2"
+	fi
+}
+
+if [ "$(getprop fas-rs-installed)" = "" ]; then
+	local_print "请先安装fas-rs再安装此插件" "Please install fas-rs first"
+	abort
+fi
