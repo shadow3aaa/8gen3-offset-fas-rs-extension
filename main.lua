7b4899986174f9c0660722ad0e4fc2328992b6a5
shadow3aaa <shadow3aaa@qq.com>
2024-09-27 21:15:49 +0800

    refactor: 调整策略 原神

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ end
 function load_fas(pid, pkg)
     if (pkg == "com.miHoYo.Yuanshen" or pkg == "com.miHoYo.ys.mi" or pkg == "com.miHoyo.ys.bilibili" or pkg == "com.miHoYo.GenshinImpact")
     then
-        set_offset(-700000, -100000, -800000, 0)
+        set_offset(0, 0, -800000, 0)
     elseif (pkg == "com.tencent.tmgp.sgame" or pkg == "com.levelinfinite.sgameGlobal")
     then
         set_offset(-600000, 0, -700000, 0)
